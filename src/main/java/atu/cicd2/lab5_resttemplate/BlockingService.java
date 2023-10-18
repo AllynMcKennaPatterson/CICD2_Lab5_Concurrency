14eca5b521297f4eafd91345ad11915e9bce8c4a
G00392765@atu.ie <G00392765@atu.ie>
2023-10-18 09:46:00 +0100

    Added fetchDataBlocking method

--- src/main/java/atu/cicd2/lab5_resttemplate/BlockingService.java
+++ src/main/java/atu/cicd2/lab5_resttemplate/BlockingService.java
@@ -4,4 +4,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BlockingService {
+
+    public String fetchDataBlocking() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
