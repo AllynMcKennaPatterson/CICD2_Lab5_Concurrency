75496602dc1917c1fe4ffd3dce7a9bc4739e7164
G00392765@atu.ie <G00392765@atu.ie>
2023-10-18 09:36:21 +0100

    Initial commit

---
+++ src/main/java/atu/cicd2/lab5_resttemplate/Lab5RestTemplateApplication.java
@@ -0,0 +1,13 @@
+package atu.cicd2.lab5_resttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5RestTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab5RestTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/atu/cicd2/lab5_resttemplate/Lab5RestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.cicd2.lab5_resttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5RestTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
